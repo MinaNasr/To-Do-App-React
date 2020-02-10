81aed8275c0ce071dbb9bb08e5d2355666ae6305
Mina Hemaya <G523507@mph.morpho.com>
2020-02-11 01:02:18 +0200

    add pagination to tasks and posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,65 +1,60 @@
 import React , {Component} from 'react';
 import axios from 'axios';
-
+import Pagination from '../shared/Pagination/Pagination';
+import ListPosts from '../ListPosts/ListPosts';
 export default class Posts extends Component {
     state = {
-        tasks: [],
+        posts: [],
         currentPage: 1,
-        tasksPerPage: 10
+        postsPerPage: 10
     }
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts').then(res=>{
             console.log('res: ', res);
+            if (res.data.length) {
+                this.setState({
+                    posts: res.data
+                })
+            }
         })
     }
+    paginate = (pageNumber)=>{
+        this.setState({
+            currentPage: pageNumber
+        })
+    }
+
+    nextPage = (Pages)=>{
+        console.log('pages: ', Pages)
+        console.log('pages: ', this.state.currentPage)
+        if(this.state.currentPage < Pages){
+            this.setState({
+                currentPage: this.state.currentPage + 1
+            })
+        }
+        console.log('stattttt: ' ,this.state)
+    }
+
+    previousPage = ()=>{
+        if(this.state.currentPage > 1){
+            this.setState({
+                currentPage: this.state.currentPage - 1
+            })
+        }
+    }
     render(){
+        const lastPostIndex = this.state.currentPage * this.state.postsPerPage;
+        const firstPostIndex = lastPostIndex - this.state.postsPerPage;
+        const currentPosts = this.state.posts.slice(firstPostIndex,lastPostIndex)
         return(
             <div>
-                <div className="row">
-                    <div className="col s12">
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                        <span className="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                        <span className="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                        <span className="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                <ListPosts posts = {currentPosts}/>
+                <Pagination itemsPerPage={this.state.postsPerPage} 
+                            totalItems={this.state.posts.length} 
+                            paginate={this.paginate}
+                            nextPage={this.nextPage}
+                            previousPage={this.previousPage}
+                            currentPage={this.state.currentPage}/>
             </div>
         )
     }
